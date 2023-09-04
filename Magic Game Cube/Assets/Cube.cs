2d31379a204a88320fc847e783a8e0e46ee2f47b
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-09-04 10:57:24 -0400

    testing debug lines

---
+++ Magic Game Cube/Assets/Cube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello I'm writting to the debug console");
+        Debug.LogError("Logging an Error message");
+        Debug.LogWarning("Logging a Warning message");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
